ada4f0011c42ee0fa38dfef7d82290473193080a
giant-paw <giantpawbusiness@gmail.com>
2024-06-22 12:30:45 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiHTf_7zfQelWux1Xrjb4cPSPOiT0X-CYvlw&s' alt="Client" className="gambar-kartu" />
+        <h1>Giant Prakoso</h1>
+        <p>Undergraduate Student at Muhammadiyah University of Yogyakarta.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
